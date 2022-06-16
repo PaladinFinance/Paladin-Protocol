0b85703c1b7d3d6a65cfed831a6f2aadeee20fea
Valentin <vigervalentin@gmail.com>
2022-06-16 18:26:23 -0400

    2-step admin

--- contracts/utils/Admin.sol
+++ contracts/utils/Admin.sol
@@ -9,8 +9,14 @@ contract Admin {
     /** @notice (Admin) Event when the contract admin is updated */
     event NewAdmin(address oldAdmin, address newAdmin);
 
+    /** @notice (Admin) Event when the contract pendingAdmin is updated */
+    event NewPendingAdmin(address oldPendingAdmin, address newPendingAdmin);
+
     /** @dev Admin address for this contract */
-    address internal admin;
+    address public admin;
+
+    /** @dev Pending admin address for this contract */
+    address public pendingAdmin;
     
     modifier adminOnly() {
         //allows only the admin of this contract to call the function
@@ -18,15 +24,32 @@ contract Admin {
         _;
     }
 
-        /**
-    * @notice Set a new Admin
-    * @dev Changes the address for the admin parameter
-    * @param _newAdmin address of the new Controller Admin
-    */
-    function setNewAdmin(address _newAdmin) external adminOnly {
-        address _oldAdmin = admin;
-        admin = _newAdmin;
+    error CannotBeOwner();
+    error CallerNotpendingAdmin();
+    error AdminZeroAddress();
 
-        emit NewAdmin(_oldAdmin, _newAdmin);
+    
+    function transferOwnership(address newAdmin) external adminOnly {
+        if(newAdmin == address(0)) revert AdminZeroAddress();
+        if(newAdmin == admin) revert CannotBeOwner();
+        address oldPendingAdmin = pendingAdmin;
+
+        pendingAdmin = newAdmin;
+
+        emit NewPendingAdmin(oldPendingAdmin, newAdmin);
+    }
+
+    function acceptAdmin() external {
+        if(pendingAdmin == address(0)) revert AdminZeroAddress();
+        if(msg.sender != pendingAdmin) revert CallerNotpendingAdmin();
+        address newAdmin = pendingAdmin;
+        address oldAdmin = admin;
+        admin = newAdmin;
+
+        emit NewAdmin(oldAdmin, newAdmin);
+
+        pendingAdmin = address(0);
+
+        emit NewPendingAdmin(newAdmin, address(0));
     }
 }
